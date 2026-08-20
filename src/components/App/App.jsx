6722f4d8cb261b39202d370bbc8e1e8b17e20189
Ivan Cervantes <153766241+Ivancvnts@users.noreply.github.com>
2026-08-19 23:08:41 -0600

    feat: Created hero component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 
 import Header from '../Header/Header';
+import Hero from '../Hero/Hero';
 
 function App() {
   return (
     <>
       <div className="app">
         <Header></Header>
+        <main>
+          <Hero></Hero>
+        </main>
       </div>
     </>
   );
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+
+function Hero() {
+  const [query, setQuery] = useState('');
+  return (
+    <section className="hero app__section">
+      <a
+        className="hero__api-reference"
+        href="https://rawg.io/apidocs#why-rawg-api"
+      >
+        BASE DE DATOS · RAWG
+      </a>
+      <h1 className="hero__title">Apila los juegos que sí importan</h1>
+      <p className="hero__description">
+        Busca entre miles de títulos, revisa notas y plataformas, y guarda los
+        que quieras jugar en tu pila personal.
+      </p>
+      <form className="hero__form" action="" id="search" name="search">
+        <div className="hero__search-container">
+          <div className="hero__searchbar">
+            <span className="hero__search-decoration">{'>'}</span>
+            <input
+              className="hero__search-input"
+              type="text"
+              id="game-search"
+              name="game-search"
+              placeholder="Busca un juego..."
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+          </div>
+          <button className="hero__search-button" type="submit" value={query}>
+            BUSCAR
+          </button>
+        </div>
+
+        <div className="hero__tags">
+          <button className="hero__tag" type="submit" value="Roguelike">
+            Roguelike
+          </button>
+          <button className="hero__tag" type="submit" value="RPG">
+            RPG
+          </button>
+          <button className="hero__tag" type="submit" value="Indie">
+            Indie
+          </button>
+          <button className="hero__tag" type="submit" value="Rockstar">
+            Rockstar
+          </button>
+          <button className="hero__tag" type="submit" value="Metactritic 95+">
+            Metacritic 95+
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default Hero;
