4392727c6985b90346d5eebd2e221c113110f970
Ivan Cervantes <153766241+Ivancvnts@users.noreply.github.com>
2026-08-23 20:23:25 -0600

    feat: added footer component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Header from '../Header/Header';
 import Hero from '../Hero/Hero';
 import GamesGrid from '../GamesGrid/GamesGrid';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Hero></Hero>
           <GamesGrid></GamesGrid>
         </main>
+        <Footer></Footer>
       </div>
     </>
   );
